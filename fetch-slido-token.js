de902fb874a3a337030b1bc251d2c7065bfecce0
Kevin Cui <krazycavin@gmail.com>
2019-04-19 16:47:24 +0200

    Add js to fetch token

---
+++ fetch-slido-token.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const url = process.argv[2]
+  const isheadless = true;
+  const chrome = '/usr/bin/chromium';
+  const privacybtn='#app > div.privacy-manager.privacy-manager__banner.privacy-manager--no-consent-action > button > svg';
+  const questionlist='#app > div:nth-child(2) > div > div > div > div > div.app__content > div > div > sda-live > div > div > sda-questions > sda-question-list'
+
+  const browser = await puppeteer.launch({executablePath: chrome, headless: isheadless});
+  const page = await browser.newPage();
+  await page.goto(url, {timeout: 60000, waitUntil: 'domcontentloaded'});
+  await page.waitFor(questionlist);
+  await page.click(privacybtn);
+
+  let cookie = await page.cookies();
+  let token = cookie.filter(c => c['name'] === 'Slido.EventAuthTokens');
+  console.log(token[0]['value']);
+
+  await browser.close();
+})();
